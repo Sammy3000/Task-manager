2c71119200afe2e863e2e229d2685dbcfdc51d22
Sammy3000 <towettsammy30@gmail.com>
2023-10-20 12:23:44 +0300

    Modify form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,11 +8,12 @@ const Form = ({todos,setTodos,setInputText,inputText}) => {
     const submitTodoHandler=e=>{
         e.preventDefault();
         setTodos(...todos,{text:inputText, completed:false,id:Math.random()*1000})
+        setInputText('')
     }
 
   return (
     <form>
-    <input onChange={inputTextHandler} type="text" className="todo-input" />
+    <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
     <button onClick={submitTodoHandler} className="todo-button" type="submit">
       <i className="fas fa-plus-square"></i>
     </button>
