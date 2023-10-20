268d61ac4b01799efb4859e3002d38f8a5e1cfee
Sammy3000 <towettsammy30@gmail.com>
2023-10-20 07:41:23 +0300

    Modify components

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <div>My main component</div>
+    </>
+  )
+}
+
+export default App
--- src/Components/TodoApp.js
+++
@@ -1,9 +0,0 @@
-const TodoApp = () => {
-    return (
-      <>
-        <h1>Hello world!</h1>
-        <p>I am in a React Component!</p>
-      </>
-    );
-  };
-  export default TodoApp;
