e54b2e25ca13aa91f77a49419414437201bc6667
Sammy3000 <towettsammy30@gmail.com>
2023-10-20 14:05:31 +0300

    Modify todo component

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>Todo</div>
+    <div className='todo'>
+        <li className='todo-item'>Heyy</li>
+        <button className='complete-btn'>
+            <i className='fas fa-check'></i>
+        </button>
+        <button className='trash-btn'>
+            <i className='fas fa-trash'></i>
+        </button>
+
+    </div>
   )
 }
 
