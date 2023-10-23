f862c768954599146f043c30131df0441e339cf8
Sammy3000 <towettsammy30@gmail.com>
2023-10-23 10:26:59 +0300

    Call status handler on the select tag

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -22,7 +22,7 @@ const Form = ({todos,setTodos,setInputText,inputText}) => {
       <i className="fas fa-plus-square"></i>
     </button>
     <div className="select">
-      <select name="todos" className="filter-todo">
+      <select onChange={statusHandler} name="todos" className="filter-todo">
         <option value="all">All</option>
         <option value="completed">Completed</option>
         <option value="uncompleted">Uncompleted</option>
